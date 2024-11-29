a501b41929b1f5a6318218d2884de3a00906557c
Moshe Swed <mswed@beapot.com>
2024-11-29 18:07:51 -0500

    sumValues

--- tree.js
+++ tree.js
@@ -15,21 +15,36 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
-  }
+    let total = 0;
+    if (this.root === null) return total;
 
-  /** countEvens(): count all of the nodes in the tree with even values. */
+    const toVisitStack = [this.root];
+    while (toVisitStack.length) {
+      const current = toVisitStack.pop();
+      total += current.val;
 
-  countEvens() {
-
-  }
-
-  /** numGreater(lowerBound): return a count of the number of nodes
-   * whose value is greater than lowerBound. */
-
-  numGreater(lowerBound) {
-
+      if (current.children.length) {
+        for (let child of current.children) {
+          toVisitStack.push(child);
+        }
+      }
+    }
+    return total;
   }
 }
+
+/** countEvens(): count all of the nodes in the tree with even values. */
+
+// countEvens() {
+//
+// };
+
+/** numGreater(lowerBound): return a count of the number of nodes
+ * whose value is greater than lowerBound. */
+
+// numGreater(lowerBound) {
+//
+// }
+// }
 
 module.exports = { Tree, TreeNode };
